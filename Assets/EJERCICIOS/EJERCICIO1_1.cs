d658a352273a0fc0c22fc6d04aa0839e13ed4136
richi200405 <richi200405@gmail.com>
2024-09-12 19:23:32 +0200

    ejercicios2.1

--- Assets/EJERCICIOS/EJERCICIO1_1.cs
+++ Assets/EJERCICIOS/EJERCICIO1_1.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EJERCICIO1_1 : MonoBehaviour
 {
+
+    [SerializeField] string nombre = "Ricardo";
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Hola mundo");
+        Debug.Log("Este es el primer videojuego de " + nombre + " .Estoy aprendiendo C#");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //repetir a cada frame que se genera el mensaje 
+        Debug.Log("Ha pasado un frame");
     }
 }
