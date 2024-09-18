4103b7a2290954aa0de16768ed6dea573b02e0c5
richi200405 <richi200405@gmail.com>
2024-09-18 18:11:05 +0200

    ejercicios 3.7

--- Assets/EJERCICIOS/3/ejercicios3_6.cs
+++ Assets/EJERCICIOS/3/ejercicios3_6.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ejercicios3_5 : MonoBehaviour
+public class ejercicios3_6 : MonoBehaviour
 {
-    [SerializeField] float radio = 0;
+    [SerializeField] int velocidad = 0;
     // Start is called before the first frame update
     void Start()
     {
-        float longitud;
-        longitud = 2*3.14f*radio ;
-        Debug.Log(longitud);
-        float area = 3.14f*(radio*radio);
-        Debug.Log(area);
-
+        float conversion = velocidad / 3.6f;
+        Debug.Log(conversion);
     }
 
     // Update is called once per frame
--- Assets/EJERCICIOS/3/ejercicios3_7.cs
+++ Assets/EJERCICIOS/3/ejercicios3_7.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ejercicios3_5 : MonoBehaviour
+public class ejercicios3_7 : MonoBehaviour
 {
-    [SerializeField] float radio = 0;
+    [SerializeField]float Basse = 0;
+    [SerializeField] float Altura = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        float longitud;
-        longitud = 2*3.14f*radio ;
-        Debug.Log(longitud);
-        float area = 3.14f*(radio*radio);
-        Debug.Log(area);
-
+        float areass = (Basse * Altura) /2;
+        Debug.Log(areass);
     }
 
     // Update is called once per frame
